405b41009e90868afd1f4cc2c31db4f25e235c66
RoachCoach <panjenambao@gmail.com>
2021-07-22 16:33:17 -0500

    added some explanation tags to html

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     //display current day & time.
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-    //Assign saveBtn click listener for user input and time stamp??
+    //Assign saveBtn click listener for user input and time stamp.
     $(".saveBtn").on("click", function () {
         //get nearby values.
         console.log(this);
@@ -14,7 +14,7 @@ $(document).ready(function () {
         //set items in local storage.
         localStorage.setItem(time, text);
     })
-    //load any saved data from LocalStorage - do this for each hour created.
+    //load any saved data from LocalStorage for each slot.
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
     $("#hour11 .description").val(localStorage.getItem("hour11"));
